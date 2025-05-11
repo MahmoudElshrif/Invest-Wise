ce7be1741bb27bfed5c692c98b3e9a4c89cad116
mahmoudElshrif <mahmoudfathyelshrif@gmail.com>
2025-05-12 00:53:16 +0300

    all ui pages

--- Main.java
+++ Main.java
@@ -1,24 +1,16 @@
-import db.UserDB;
-import service.AssetService;
-import service.ReportService;
-import service.ZakatService;
-import ui.SignUpPage;
-
-import java.util.List;
-import java.util.Scanner;
+import UI.SignUpPage;
 
 /**
  * Entry point for the Wealth Wise application.
  */
 public class Main {
 
-    /**
-     * Launches the Sign-Up page for the user.
-     *
-     * @param args command-line arguments
-     */
-    public static void main(String[] args) {
-        SignUpPage signUpPage = new SignUpPage();
-        signUpPage.main(args);
-    }
+	/**
+	 * Launches the Sign-Up page for the user.
+	 *
+	 * @param args command-line arguments
+	 */
+	public static void main(String[] args) {
+		SignUpPage signUpPage = new SignUpPage();
+	}
 }
